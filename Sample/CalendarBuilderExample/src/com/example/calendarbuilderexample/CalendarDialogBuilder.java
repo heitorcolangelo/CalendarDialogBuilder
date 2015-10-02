345de56ef6184a4dfa0c2022c4c1708f064960b8
Heitor Colangelo <heitorcolangelo@gmail.com>
2015-10-02 11:30:55 -0300

    Update for initial date, max and min date feature

--- Sample/CalendarBuilderExample/src/com/example/calendarbuilderexample/CalendarDialogBuilder.java
+++ Sample/CalendarBuilderExample/src/com/example/calendarbuilderexample/CalendarDialogBuilder.java
@@ -16,104 +16,130 @@ import android.widget.LinearLayout;
 public class CalendarDialogBuilder {
 
 	public interface OnDateSetListener {
-		void onDateSet(int Year, int Month, int Day);
-	}
 
-	private OnDateSetListener dateSetlistener;
-	private Context context;
-	
-	// To hold the values of year, month and day.
-	public int YEAR, MONTH, DAY;
+        void onDateSet(int Year, int Month, int Day);
+    }
 
-	// Constructor.
-	public CalendarDialogBuilder(Context ctx, OnDateSetListener lisetener) {
-		this.context = ctx;
-		this.dateSetlistener = lisetener;
-	}
+    private OnDateSetListener dateSetlistener;
+    private Context context;
+    private long initialDate;
+    private CalendarView mCv;
+    private AlertDialog.Builder alertBuilder;
 
-	public AlertDialog.Builder returnDialog()
-	{
-		// Inflating the layour.
-		LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService
-	            (Context.LAYOUT_INFLATER_SERVICE);
-		LinearLayout ll = (LinearLayout)inflater.inflate(R.layout.calendar_dialog, null, false);
+    // To hold the values of year, month and day.
+    public int YEAR, MONTH, DAY;
 
-		// Getting the CalendarView.
-		CalendarView cv = (CalendarView) ll.getChildAt(0);
-		
-		// Setting the listener.
-		cv.setOnDateChangeListener(new OnDateChangeListener() { 
-		
-		      @Override 
-		      public void onSelectedDayChange(CalendarView view, int year, int month,
-		              int dayOfMonth) {
-		    	  
-			    	YEAR = year;
-			    	MONTH = month;
-			    	DAY = dayOfMonth;
- 
-		      } 
-		  });
+    // Constructor.
+    public CalendarDialogBuilder(Context ctx, OnDateSetListener listener) {
+        this.context = ctx;
+        this.dateSetlistener = listener;
+        this.alertBuilder = returnDialog();
+    }
 
-		// Configuring the calendar view.		
-		configCalendarView(cv);
-		
-		// Creating the alert dialog that will display our LinearLayout with the calendar view.
-		AlertDialog.Builder aDBuilder = new AlertDialog.Builder(context)
-		  .setView(ll)
-		  .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-		      public void onClick(DialogInterface dialog, int whichButton) {
-		    	  dateSetlistener.onDateSet(YEAR, MONTH, DAY); 
-		      } 
-		  })
-		  .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-		      public void onClick(DialogInterface dialog, int whichButton) { 
-		    	  dateSetlistener.onDateSet(0,0,0);  
-		      } 
-		  });
-		
-		return aDBuilder;
-		  
-	}
-	
-	// This method will be called from the activity.
-	public void showCalendar()
-	{
-		returnDialog().show();
-	}
-	
-	// Here you can configure your CalendarView.
-	@SuppressLint("NewApi") 
-	private void configCalendarView(CalendarView cv){
+    public CalendarDialogBuilder(Context ctx, OnDateSetListener listener, long initialDate) {
+        this.context = ctx;
+        this.dateSetlistener = listener;
+        this.initialDate = initialDate;
+        this.alertBuilder = returnDialog();
+    }
 
-		// sets whether to show the week number. 
-        cv.setShowWeekNumber(false);
-		
-		// sets the first day of week according to Calendar. 
+    public AlertDialog.Builder returnDialog() {
+        // Inflating the layour.
+        LayoutInflater inflater = (LayoutInflater) context.getApplicationContext().getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.calendar_dialog, null, false);
+
+        // Getting the CalendarView.
+        mCv = (CalendarView) ll.getChildAt(0);
+
+        // Setting the listener.
+        mCv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month,
+                                            int dayOfMonth) {
+                YEAR = year;
+                MONTH = month;
+                DAY = dayOfMonth;
+            }
+        });
+
+        // Configuring the calendar view.
+        configCalendarView();
+
+        // Creating the alert dialog that will display our LinearLayout with the calendar view.
+        AlertDialog.Builder aDBuilder = new AlertDialog.Builder(context)
+                .setView(ll)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dateSetlistener.onDateSet(YEAR, MONTH, DAY);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dateSetlistener.onDateSet(0, 0, 0);
+                    }
+                });
+
+        return aDBuilder;
+
+    }
+
+    // This method will be called from the activity.
+    public void showCalendar() {
+        alertBuilder.show();
+    }
+
+    public void setStartDate(long startDate) {
+        mCv.setMinDate(startDate);
+    }
+
+    public void setEndDate(long endDate) {
+        mCv.setMaxDate(endDate);
+    }
+
+    // Here you can configure your CalendarView.
+    @SuppressLint("NewApi")
+    private void configCalendarView() {
+
+        // sets whether to show the week number.
+        mCv.setShowWeekNumber(false);
+
+        // sets the first day of week according to Calendar.
         // here I set Sunday as the first day of the Calendar
-		cv.setFirstDayOfWeek(1);
+        mCv.setFirstDayOfWeek(1);
 
-		// sets whether to show the week number. 
-        cv.setShowWeekNumber(false);
-          
-		// sets the background color.
-		cv.setBackgroundColor(context.getResources().getColor(R.color.a200_orange));
-		
-		// theses methods are API 16+ only.	
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-		{
-			//The background color for the selected week.
-			cv.setSelectedWeekBackgroundColor(context.getResources().getColor(R.color.a100_orange));
+        // sets whether to show the week number.
+        mCv.setShowWeekNumber(false);
 
-			//sets the color for the vertical bar shown at the beginning and at the end of the selected date.
-			cv.setSelectedDateVerticalBar(new ColorDrawable(R.color.green));	
+        // if there's a initial date, set it.
+        if (initialDate != 0) {
+            mCv.setDate(initialDate, true, false);
 
-			//sets the color for the dates of an unfocused month.  
-	        cv.setUnfocusedMonthDateColor(context.getResources().getColor(R.color.grey));
-	      
-	        //sets the color for the separator line between weeks. 
-	        cv.setWeekSeparatorLineColor(context.getResources().getColor(R.color.darkgreen));			
-		}
-	}
+            Date dateToSet = new Date(initialDate);
+            YEAR = dateToSet.getYear();
+            MONTH = dateToSet.getMonth();
+            DAY = dateToSet.getDay();
+
+        }
+
+        // sets the background color.
+        mCv.setBackgroundColor(context.getResources().getColor(R.color.a200_orange));
+
+        // theses methods are API 16+ only.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            //The background color for the selected week.
+            mCv.setSelectedWeekBackgroundColor(context.getResources().getColor(R.color.a100_orange));
+
+            //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+            mCv.setSelectedDateVerticalBar(new ColorDrawable(context.getResources().getColor(R.color.green)));
+
+            //sets the color for the dates of an unfocused month.
+            mCv.setUnfocusedMonthDateColor(context.getResources().getColor(R.color.grey));
+
+            //sets the color for the separator line between weeks.
+            mCv.setWeekSeparatorLineColor(context.getResources().getColor(R.color.darkgreen));
+        }
+    }
 	
 }
